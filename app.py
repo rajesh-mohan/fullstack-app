cf8ab68217118f07cc45d23e550e014873dd8345
Rajesh Mohan <rajeshmohan@live.com>
2021-12-21 00:46:24 +0530

    passing UX vars

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", ux={'light_theme':True})
 
 @app.route("/page1")
 def page1():
-    return render_template("page1.html", **{'greeting': 'Hello from Flask!'})
+    return render_template("page1.html", **{'greeting': 'Hello from Flask!'}, ux={'light_theme':True})
 
 @app.route("/page2")
 def page2():
-    return render_template("page2.html")
+    return render_template("page2.html", ux={'admin':True})
