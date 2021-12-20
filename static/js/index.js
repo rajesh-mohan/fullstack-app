7d63fcaa01daffa7b60e0bc2d218c2e211ecb37c
Rajesh Mohan <rajeshmohan@live.com>
2021-12-21 00:46:58 +0530

    moving Vue instance to HTML for Jinja rendering

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,16 @@
-const vm = new new Vue({
-    el: '#app',
-    // data: {
-    //   greeting: 'Hello, Vue!'
-    // },      
-    vuetify: new Vuetify(),
-  })
+// const vm = new Vue({
+//     el: '#app',
+//     data: {
+//       greeting: 'Hello, Vue!',
+//       notifications: [
+//         'Mike John Responded to your email',
+//         'You have 5 new tasks',
+//         'You\'re now friends with Andrew',
+//         'Another Notification',
+//         'Another one',
+//       ],      
+//     },      
+//     vuetify: new Vuetify({
+//       theme: { dark: true },
+//     }),
+//   })
