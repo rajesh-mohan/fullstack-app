aafdf5bc4d42dfb9c4cea4a657ceb0385d5325be
Rajesh Mohan <rajeshmohan@live.com>
2021-12-20 23:41:10 +0530

    adding multiple routes

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("cdn-vuetify-vue.html", **{'greeting': 'Hello from Flask!'})
+    return render_template("home.html")
+
+@app.route("/page1")
+def page1():
+    return render_template("page1.html", **{'greeting': 'Hello from Flask!'})
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
